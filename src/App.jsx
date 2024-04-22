2a7a617caf9efbbb48c7001e5e3393eb7897e629
tinaraissa88 <raissafabriciolima3@gmail.comm>
2024-04-22 20:25:45 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className=' w-88	place-content-center justify-center flex'>
+      <h1 className='text-orange-600 text-5xl font-bold place-content-center justify-center flex border-8 border-amber-500 rounded-2xl	bg-orange-100'> O Lorax: Em Busca da Trúfula Perdida</h1>
+    </div>
+  )
+}
+
+export default App
+
