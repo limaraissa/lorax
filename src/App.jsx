32b150040ed45779b83f99056daf6c184eb5df9e
tinaraissa88 <raissafabriciolima3@gmail.comm>
2024-04-22 20:48:29 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Header from './componentes/Header'
+import Inicio from './componentes/Inicio'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
   return (
-    <div className=' w-88	place-content-center justify-center flex'>
-      <h1 className='text-orange-600 text-5xl font-bold place-content-center justify-center flex border-8 border-amber-500 rounded-2xl	bg-orange-100'> O Lorax: Em Busca da Trúfula Perdida</h1>
+    <div>
+      <Header/>
+      <Inicio/>
     </div>
+    
+    
   )
 }
 
-export default App
 
---
+++ src/componentes/Inicio.jsx
@@ -0,0 +1,11 @@
+import loraxS from "../assets/lorax1"
+import chapel from "../assets/chapel"
+
+export default function Inicio() {
+  return (
+    <div>
+      <img src={loraxS} alt="primeira img do lorax" />
+      <img src={chapel} alt="moço principal" />
+    </div>
+  )
+}
